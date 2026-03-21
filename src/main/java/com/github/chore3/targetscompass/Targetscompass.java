57058e33321ab1eb09efa78c17ab9d0a3adaccf6
chore_3 <anon408chore@gmail.com>
2026-03-21 17:27:09 +0900

    refs: #1

---
+++ src/main/java/com/github/chore3/targetscompass/Targetscompass.java
@@ -0,0 +1,11 @@
+package com.github.chore3.targetscompass;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod("targetscompass")
+public class Targetscompass {
+    public static final String MOD_ID = "targetscompass";
+    public static final Logger LOGGER = LogUtils.getLogger();
+}
