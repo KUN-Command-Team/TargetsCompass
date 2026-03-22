5d96e5105c1b5dea7cedf71e0b44c5fe36f6602a
chore_3 <anon408chore@gmail.com>
2026-03-23 04:03:51 +0900

    add: 今後利用するクラスを追加

---
+++ src/main/java/com/github/chore3/targetscompass/server/item/tracker/TargetMapBuilder.java
@@ -0,0 +1,9 @@
+package com.github.chore3.targetscompass.server.item.tracker;
+
+import net.minecraft.world.entity.player.Player;
+
+import java.util.Map;
+
+Map<String, Player[]>
+public class TargetMapBuilder {
+}
