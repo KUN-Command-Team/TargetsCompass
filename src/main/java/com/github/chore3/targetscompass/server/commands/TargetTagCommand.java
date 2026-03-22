02479ce0c7797f58e1e818eeab2659ff6f4d68a9
chore_3 <anon408chore@gmail.com>
2026-03-22 21:41:10 +0900

    feat: 手持ちのアイテムにTargetTagを付けるコマンドを追加

--- src/main/java/com/github/chore3/targetscompass/server/commands/TargetTagCommand.java
+++ src/main/java/com/github/chore3/targetscompass/server/commands/TargetTagCommand.java
@@ -1,27 +1,29 @@
 package com.github.chore3.targetscompass.server.commands;
 
+import com.github.chore3.targetscompass.common.item.TargetCompassNbt;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
 public class TargetTagCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher){
-    dispatcher.register(Commands.literal("targettag")
-        .executes(context -> {
-            CommandSourceStack source = context.getSource();
-            Player player = source.getPlayerOrException();
-            ItemStack itemInHand = player.getMainHandItem();
+        var targetTag = Commands.literal("targettag")
+                .requires(source -> source.hasPermission(2))
+                .then(Commands.argument("name", StringArgumentType.word())
+                .executes(context -> applyTargetTag(context, StringArgumentType.getString(context, "name"))));
+        dispatcher.register(targetTag);
+    }
 
-            if(!itemInHand.isEmpty()){
-                source.sendSuccess(() -> Component.literal(itemInHand.getHoverName().getString()), false);
-            } else {
-                source.sendSuccess(() -> Component.literal("none"), false);
-            }
-            return 1;
-        })
-    );
+    private static int applyTargetTag(CommandContext<CommandSourceStack> context, String message) throws CommandSyntaxException {
+        CommandSourceStack source = context.getSource();
+        Player player = source.getPlayerOrException();
+        ItemStack itemInHand = player.getMainHandItem();
+        TargetCompassNbt.targetTagSet(itemInHand, message);
+        return 1;
     }
 }
